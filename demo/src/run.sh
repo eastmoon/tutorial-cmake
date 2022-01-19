9d094a3b9b3237fd5ac1230cba6f6951c93049d7
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2022-01-19 13:54:11 +0800

    feat: add remove legacy out directory, and recreate out directory

--- demo/src/run.sh
+++ demo/src/run.sh
@@ -1,5 +1,6 @@
 if [ -d ${1} ]; then
-    [ ! -d ./${1}/out ] && mkdir -p ./${1}/out
+    [ -d ./${1}/out ] && rm -rf ./${1}/out
+    mkdir -p ./${1}/out
     cmake -S ./${1} -B ./${1}/out
     make -C ./${1}/out
     echo ">> Execute example"
